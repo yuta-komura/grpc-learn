57535bb4c0bb488286028359a06802f8ff3447a4
yuta-komura <esfgshdfsd@gmail.com>
2023-10-27 02:03:22 +0900

    Add RegisterGreetingServiceServerの実装

---
+++ cmd/server/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	hellopb "grpc-learn/pkg/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 1. 8080番portのListenerを作成
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	// 2. gRPCサーバーを作成
+	s := grpc.NewServer()
+
+	// 3. gRPCサーバーにGreetingServiceを登録
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	// 3. 作成したgRPCサーバーを、8080番ポートで稼働させる
+	go func() {
+		log.Printf("start gPRC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	// 4. Ctrl+Cが入力されたらGraceful shutdownされるようにする
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	// リクエストからnameフィールドを取り出して
+	// "Hello, [名前]!"というレスポンスを返す
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
